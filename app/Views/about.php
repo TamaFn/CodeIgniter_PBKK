c31b29988f38fc63a73c9eafd48428e64157a2ea
TamaFn <sandya.fransisna@gmail.com>
2023-11-03 21:06:33 +0700

    TugasCodeIgniterPBKK

---
+++ app/Views/about.php
@@ -0,0 +1,35 @@
+<!DOCTYPE html>
+<html>
+
+<head>
+    <title>About Us</title>
+    <style>
+        body {
+            font-family: Arial, sans-serif;
+            background-color: #f2f2f2;
+        }
+
+        h1 {
+            color: #333;
+            text-align: center;
+            margin-top: 50px;
+        }
+
+        p {
+            color: #666;
+            font-size: 18px;
+            line-height: 1.5;
+            margin: 30px auto;
+            max-width: 600px;
+            text-align: center;
+        }
+    </style>
+</head>
+
+<body>
+    <h1>About Me</h1>
+    <p>My name is <?php echo "Sandyatama Fransisna Nugraha" ?></p>
+    <p>NRP : <?php echo "502521196" ?></p>
+</body>
+
+</html>
